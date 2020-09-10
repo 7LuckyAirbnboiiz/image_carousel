b9d6546290b20203c5b24e796095409865c9b6fd
Yui Davidson <yuidavidson@gmail.com>
2020-09-09 20:53:29 -0700

    Commented out mongo query

--- server/index.js
+++ server/index.js
@@ -18,18 +18,18 @@ app.use('/rooms/:id', expressStaticGzip(path.join(__dirname, '../public')));
 
 // get request original mongodb
 
-app.get('/suggestedListings', (req, res) => {
-  console.log('get req working!');
-  model.getListings((error, listings) => {
-    if (error) {
-      console.log('server down');
-      res.status(400).send(error);
-    } else {
-      console.log('GET received!');
-      res.status(200).send(listings);
-    }
-  });
-});
+// app.get('/suggestedListings', (req, res) => {
+//   console.log('get req working!');
+//   model.getListings((error, listings) => {
+//     if (error) {
+//       console.log('server down');
+//       res.status(400).send(error);
+//     } else {
+//       console.log('GET received!');
+//       res.status(200).send(listings);
+//     }
+//   });
+// });
 
 // new get request for PostgreSQL
 
