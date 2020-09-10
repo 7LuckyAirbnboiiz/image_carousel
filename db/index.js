e6f7ef1176d8b969affcf1a2b9a0b9e50c4baf52
Yui Davidson <yuidavidson@gmail.com>
2020-09-09 20:49:43 -0700

    Commented out mongo connection

--- db/index.js
+++ db/index.js
@@ -1,34 +1,34 @@
-const mongoose = require('mongoose');
+// const mongoose = require('mongoose');
 
-// used locally
-const mongoUrl = 'mongodb://localhost:27017/suggestedListings';
+// // used locally
+// const mongoUrl = 'mongodb://localhost:27017/suggestedListings';
 
-// used with ec2
-// const morgoUrl = 'mongdb://database/suggestedListings';
+// // used with ec2
+// // const morgoUrl = 'mongdb://database/suggestedListings';
 
-mongoose.connect(mongoUrl, { useNewUrlParser: true });
-const db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', () => {
-  console.log('Connected to database!');
-});
+// mongoose.connect(mongoUrl, { useNewUrlParser: true });
+// const db = mongoose.connection;
+// db.on('error', console.error.bind(console, 'connection error:'));
+// db.once('open', () => {
+//   console.log('Connected to database!');
+// });
 
-const listingSchema = new mongoose.Schema({
-  image: String,
-  description: String,
-  title: String,
-  rate: Number,
-  avgRating: Number,
-  numberOfRatings: Number,
-  wasLiked: Boolean,
-  superhost: Boolean,
-});
+// const listingSchema = new mongoose.Schema({
+//   image: String,
+//   description: String,
+//   title: String,
+//   rate: Number,
+//   avgRating: Number,
+//   numberOfRatings: Number,
+//   wasLiked: Boolean,
+//   superhost: Boolean,
+// });
 
-const roomsSchema = new mongoose.Schema({
-  _id: Number,
-  rooms: Array,
-});
+// const roomsSchema = new mongoose.Schema({
+//   _id: Number,
+//   rooms: Array,
+// });
 
-const Listing = mongoose.model('Listing', listingSchema);
+// const Listing = mongoose.model('Listing', listingSchema);
 
-module.exports = Listing;
+// module.exports = Listing;
